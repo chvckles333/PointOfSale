f3488b699f1d86e7300ca250c6cc2630ce040674
Unknown <cfranklin8@mail.csuchico.edu>
2017-03-30 11:50:01 -0700

    changed so input saves into cookie

--- sales.js
+++ sales.js
@@ -6,14 +6,25 @@ function addItem()
   newItem = document.getElementById("price").value;
   //IF newItem is not a number
   // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
   // update newItem to its value cast as a number
+  if(isNaN(newItem)) {
+    alert("Enter price as a number");
+    newItem = Number(newItem);
+  }
+  //OTHERWISE,
   // update runningTotal to be its value plus newItem
   // create a variable called dollars
   // call asCurrency() by with the value of runningTotal and assign the return value to dollars
   // update the innerHTML of the span with the id "subtotal" to be dollars
   // update the value of the input with the id "price" to be an empty string
   // update a cookie called "preTax" with the value of runningTotal
+  else {
+    runningTotal+=Number(newItem);
+    var dollars = asCurrency(runningTotal);
+    document.getElementById("subtotal").innerHTML=dollars;
+    document.getElementById("price").input="";
+    setCookie("preTax", runningTotal, 1);
+  }
 }
 
 //takes a number and gives a string with the number displayed as USD currency
